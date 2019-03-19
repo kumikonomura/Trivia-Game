ba7862470eb57107a84d5c85fcb6ed9440c25a56
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-03-18 23:31:46 -0700

    start page added

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+const startGame = document.getElementById('start');
+
+console.log(startGame);
+
+
+
+
+
+
+
+
+
+var questions = [{
+    question: 'At the beginning of the series, how many children do Ned and Catelyn Stark have?',
+    answers: ["4", "5", "6", "3"],
+    correctAnswer: "5",
+  }, {
+    question: 'Who is the first character in the series to be called "King in the North"?',
+    answers: ["Jon Snow", "Ned Stark", "Robb Stark", "Edmure Tully"],
+    correctAnswer: "Robb Stark",
+  }, {
+    question: 'Which of these Westeros couples is a couple off screen as well?',
+    answers: ["Richard Madden and Oona Chaplin", "Emilia Clarke and Michiel Huisman", "Nikolai Coster-Waldau and Lena Headey", "Kit Harington and Rose Leslie"],
+    correctAnswer: "Kit Harington and Rose Leslie",
+  }, {
+    question: 'Which of these characters killed Ramsey Bolton?',
+    answers: ["Jon Snow", "Arya Stark", "Sansa Stark", "Theon Greyjoy"],
+    correctAnswer: "Sansa Stark",
+  }, {
+    question: 'Which popular Disney movie featured the song, "Circle of Life"?',
+    answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+    correctAnswer: "The Lion King",
+  }, {
+    question: 'Finish this line from the Fresh Prince of Bel-Air theme song: "I whistled for a cab and when it came near, the license plate said..."',
+    answers: ["Dice", "Mirror", "Fresh", "Cab"],
+    correctAnswer: "Fresh",
+  }, {
+    question: "What was Doug's best friend's name?",
+    answers: ["Skeeter", "Mark", "Zach", "Cody"],
+    correctAnswer: "Skeeter",
+  }, {
+    question: "What was the name of the principal at Bayside High in Saved By The Bell?",
+    answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
+    correctAnswer: "Mr.Belding",
+  }];
+
+  console.log(questions);
