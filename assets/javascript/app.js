cdb0923c507e252b8c573c99dd8ca37589a2b743
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-03-20 23:27:37 -0700

    added some functions to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,168 @@
-const startGame = document.getElementById('start');
+// Variables for the ending results
+let correctAnswers =0
+let wrongAnswers = 0
+let notAnswered = 0
 
-console.log(startGame);
+// When the Start button is clicked
+const start = document.getElementById("start");
+
+const question = document.getElementById("question");
+
+const timer = document.getElementById("timer");
+
+const answerA = document.getElementById("A");
+const answerB = document.getElementById("B");
+const answerC = document.getElementById("C");
+const answerD = document.getElementById("D");
+
+// function to build the game
+function game () {}
+
+// function that will show the results when the submit button is clicked
+function showResults() {}
+
+// will display the game
+game();
+
+// When the submit button is clicked, will show results
+// submitButton.addEventListener('click', showResults);
 
 
 
 
+// sets index position of last question in the game (A,B,C,D)
+// let lastQuestionIndex = questions.length - 1
+
+// Variable for the current question
+let currentQuestion = 0
 
 
 
 
-
-var questions = [{
-    question: 'At the beginning of the series, how many children do Ned and Catelyn Stark have?',
-    answers: ["4", "5", "6", "3"],
-    correctAnswer: "5",
+let questions = [{
+    question: `In the 2012 movie, The Avengers features Captain America. What is his real name?`,
+    answers: {
+        a: "Buck Rogers", 
+        b: "Ted Rogers",
+        c: "Steve Rogers",
+        d: "Tony Stark",
+    },
+    correctAnswer: "Steve Rogers"
   }, {
-    question: 'Who is the first character in the series to be called "King in the North"?',
-    answers: ["Jon Snow", "Ned Stark", "Robb Stark", "Edmure Tully"],
-    correctAnswer: "Robb Stark",
+    question: `Who is the leader of S.H.I.E.L.D?`,
+    answers: {
+        a:   "Nick Fury", 
+        b: "Tony Stark", 
+        c: "Bruce Banner",
+        d: "Diana Prince",
+    },
+    correctAnswer: "Nick Fury"
   }, {
-    question: 'Which of these Westeros couples is a couple off screen as well?',
-    answers: ["Richard Madden and Oona Chaplin", "Emilia Clarke and Michiel Huisman", "Nikolai Coster-Waldau and Lena Headey", "Kit Harington and Rose Leslie"],
-    correctAnswer: "Kit Harington and Rose Leslie",
+    question: `Which superhero does Bruce Banner transform into?`,
+    answers: {
+        a: "Iron Man", 
+        b: "Hawkeye",
+        c: "Thor",
+        d: "Hulk",
+    },
+    correctAnswer: "The Hulk"
   }, {
-    question: 'Which of these characters killed Ramsey Bolton?',
-    answers: ["Jon Snow", "Arya Stark", "Sansa Stark", "Theon Greyjoy"],
-    correctAnswer: "Sansa Stark",
+    question: `Who is Loki's adoptive brother?`,
+    answers: {
+        a:  "Thor",
+        b: "Tony Stark", 
+        c: "Peter Parker", 
+        d: "Bruce Wayne",
+    },
+    correctAnswer: "Thor"
   }, {
-    question: 'Which popular Disney movie featured the song, "Circle of Life"?',
-    answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
-    correctAnswer: "The Lion King",
+    question: `What's the name of the mysterious blue glowing cube that Loki uses as a weapon?`,
+    answers: {
+        a: "The Orb",
+        b: "Tesseract",
+        c: "The Force",
+        d: "UV Lamp",
+    },
+    correctAnswer: "Tesseract"
   }, {
-    question: 'Finish this line from the Fresh Prince of Bel-Air theme song: "I whistled for a cab and when it came near, the license plate said..."',
-    answers: ["Dice", "Mirror", "Fresh", "Cab"],
-    correctAnswer: "Fresh",
+    question: `In Avengers: Infinity War, what is the name of the villain who's on a mission to collect the infinity stones?`,
+    answers: {
+        a: "Malekith",
+        b: "Thanos", 
+        c: "Darren Cross",
+        d: "Helmut Demo",
+    },
+    correctAnswer: "Thanos"
   }, {
-    question: "What was Doug's best friend's name?",
-    answers: ["Skeeter", "Mark", "Zach", "Cody"],
-    correctAnswer: "Skeeter",
+    question: `How does Black Widow turn the Hulk back into Bruce Banner?`,
+    answers: {
+        a: "By asking politely",
+        b: "With a text message",
+        c: "With a lullaby",
+        d: "With a special potion",
+    },
+    correctAnswer: "With a lullaby",
   }, {
-    question: "What was the name of the principal at Bayside High in Saved By The Bell?",
-    answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
-    correctAnswer: "Mr.Belding",
+    question: `What is Clint Barton's superhero name?`,
+    answers: {
+        a: "Black Panther",
+        b: "Hawkeye",
+        c: "Iron Man",
+        d: "Thor",
+    correctAnswer: "Hawkeye"
+    },
   }];
 
-  console.log(questions);
+//   console.log(questions);
+
+//   <!-- JAVASCRIPT -->
+// <!-- [ -->
+// <!-- question objects -->
+// <!-- ] -->
+
+// <!-- {questionId, isCorrect} -->
+let qTracker = 0
+function renderQuestion (index) {
+    let currentQuestionElement = document.getElementById('current-question')
+    currentQuestionElement.innerHTML = questions[index].question 
+}
+
+function nextQuestion () {
+    if (qTracker === 0) {
+        renderQuestion(qTracker)
+        renderChoices(qTracker)
+    } else {
+        renderQuestion(qTracker++)
+        renderChoices(qTracker++)
+    }
+}
+
+function renderChoices (index) {
+    let option1EL = document.getElementById('A')
+    let option2EL = document.getElementById('B')
+    let option3EL = document.getElementById('C')
+    let option4EL = document.getElementById('D')
+
+    let option1 = questions[index].answers.a
+    let option2 = questions[index].answers.b
+    let option3 = questions[index].answers.c
+    let option4 = questions[index].answers.d
+
+    option1EL.innerHTML = option1
+    option2EL.innerHTML = option2
+    option3EL.innerHTML = option3
+    option4EL.innerHTML = option4
+}
+
+let answers = document.getElementsByClassName('answer')
+
+
+// <!-- render choices (data-value = true or false) -->
+
+// <!-- whenever we click a choice -->
+// <!-- set the clicked one to look different -->
+// <!-- set all others to go back to default -->
+// <!-- modify the question object to have value for whether they answered right or wrong -->
+
+// <!-- whenever finish game button is clicked OR the timer runs out -->
+// <!-- iterate over questions, count up isCorrect -->
